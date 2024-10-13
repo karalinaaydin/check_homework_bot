cbf7b74022a609dda99ffd80a59f306b052bcb59
Karalina Aydin <karalina.komar@gmail.com>
2024-10-13 20:19:11 +0300

    fix git branches

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,6 @@
 class APIResponseError(Exception):
     """Кастомное исключение для ошибок ответа API."""
 
-<<<<<<< HEAD
 class MissingTokensError(Exception):
     """Кастомное исключение для ошибок отсутствующих токенов."""
 
@@ -10,7 +9,3 @@ class SendMessageError(Exception):
 
 class ProgramError(Exception):
     """Кастомное исключение для ошибок сбоя программы."""
-=======
-class MissingTokenError(Exception):
-    """Исключение для отсутствующих токенов."""
->>>>>>> 399cfce23bb185cd063b38ba800dbf20a3830cb7
